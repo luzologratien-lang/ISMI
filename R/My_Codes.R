125564963f7315869f91ccdf0291606c20b73c56
Gratien M. Luzolo <luzologratien@gmail.com>
2026-08-20 20:45:15 -0400

    Update My_Codes.R

--- R/My_Codes.R
+++ R/My_Codes.R
@@ -1,1 +1,5 @@
-
+library(tidyverse)
+library(zoo)
+df_current <- read.csv("3610012401_databaseLoadingData-2.csv") %>%
+filter(GEO == "Canada") %>%
+select(REF_DATE, Prices, Estimates, VALUE_current = VALUE)
